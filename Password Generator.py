ac0d39b7cac3e86ec81829f2f1c4ae6bd7424fe2
VincentMoses123 <24.vincent.moses@gmail.com>
2024-04-10 15:48:50 -0400

    First Commit

---
+++ Password Generator.py
@@ -0,0 +1,11 @@
+#Code by Vincent Moses II
+import random
+import string
+
+def generate_password(length: int=10):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(alphabet) for i in range(length))
+    return password
+
+password = generate_password()
+print("Generated Password: " + password)
