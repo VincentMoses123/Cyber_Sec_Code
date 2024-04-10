950ddcec2639246dfdb55362344a2f5ba14c8403
VincentMoses123 <24.vincent.moses@gmail.com>
2024-04-10 16:12:37 -0400

    Added Comments

--- Password Generator.py
+++ Password Generator.py
@@ -1,10 +1,11 @@
-#Code by Vincent Moses II
+# Code by Vincent Moses II
 import random
 import string
 
 def generate_password(length: int=10):
+    #Picks 10 random letters, numbers, or punctuation
     alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(alphabet) for i in range(length))
+    password = ''.join(random.choice(alphabet) for i in range(length)) # repeats picking things intill lenght reaches 10
     return password
 
 password = generate_password()
