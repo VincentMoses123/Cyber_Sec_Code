9b1f5c61d80715b33271d52bef767a7c3f9767e1
Vincent Moses <146036089+VincentMoses123@users.noreply.github.com>
2024-08-22 20:20:40 -0400

    123

--- Password_Checker_Maker.py
+++ Password_Checker_Maker.py
@@ -1,18 +1,13 @@
 import random
 import string
 import re
-import Password_Strengh
+from Password_Strengh import validate_password
+from Password_Generator import generate_password
+
 
 
 print("Welcome to the password test")
 print("In this test, the code checks the password you put in")
 print("Then if it does not recive a password it liks, it gives you a new one that is better!")
 
-import validate_password 
-password=input(str("Enter your password: "))
-is_valid=validate_password(password)
-
-if is_valid:
-    print("Valid and strong password")
-else:
-    print("Not a valid password!")
+(Password_Generator)
